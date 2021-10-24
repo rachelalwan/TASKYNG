6a484e320944ce93984fd0aaac3bf63692a0d3dd
rachelalwan <ralwan@uw.edu>
2021-10-23 19:20:34 -0700

    Added bg formatting, date, and time

---
+++ src/timeDate.js
@@ -0,0 +1,23 @@
+function showTime() {
+    const today = new Date();
+    let hourIndicator = today.getHours() / 12 === 1 ? "AM" : "PM";
+
+    let mins = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+    let seconds = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+
+    const time = (today.getHours() % 12) + ":" + mins + ":" + seconds + " " + hourIndicator;
+
+    document.getElementById("time").innerHTML = time;
+}
+
+function showDate() {
+    const today = new Date();
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'];
+
+    const date = months[today.getMonth()] + " " + today.getDay() + ", " + today.getFullYear();
+
+    document.getElementById("date").innerHTML = date;
+}
+
+setInterval(showTime,1000);
+setInterval(showDate, 1000);
